c3aaf6f161f87aabdbae85ca43500fdd9f8d10d7
syafenrideputra <syafenrii.deputra@gmail.com>
2021-09-22 16:38:38 +0700

    commit ke 5

--- main.go
+++ main.go
@@ -2,20 +2,55 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"net/http"
 )
 
 func uploadFile(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Uploading File")
+	fmt.Fprintf(w, "Uploading File\n")
+
+	//1. parse input, multipart/form-data
+	r.ParseMultipartForm (10 << 20)
+
+	//2. rerive file from posted form-data
+	file, handler, err := r.FormFile("MyFile")
+	if err !=nil {
+		fmt.Println("Error retrieving file from form-data")
+		fmt.Println(err)
+		return
+	}
+	defer file.Close()
+	fmt.Println("Uploaded File: %+v\n", handler.Filename)
+	fmt.Println("File Size: %+v\n", handler.Size)
+	fmt.Println("MIME Header: %+v\n", handler.Header)
+
+	//3. write temporary file on our server
+	tempFile, err := ioutil.TempFile("temp-images","upload-*.png")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer tempFile.Close()
+
+	fileBytes, err :=ioutil.ReadAll(file)
+	if err !=nil{
+		fmt.Println(err)
+	}
+
+	tempFile.Write(fileBytes)
+
+	//4. return wheter or not this has been successfull
+	fmt.Println(w, "Successfully Uploaded File")
+
 }
 
 func setupRoutes()  {
 	http.HandleFunc("/upload", uploadFile)
-	http.ListenAndServe(":80",nil)
+	http.ListenAndServe(":8080",nil)
 
 }
 
 func main()  {
-	fmt.Println("Go File Upload Download Ade, test commit 3, tapi belum masuk, nah ini baru masuk")
+	fmt.Println("Upload Test Webpagek")
 
 }
